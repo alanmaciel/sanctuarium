5825100c046675dae6593769aea3342084d8b90a
Alan Maciel <alan.maciel.salcedo@gmail.com>
2016-09-12 11:45:13 -0500

    Added CRUD operations to documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,30 +1,50 @@
 class DocumentsController < ApplicationController
+  before_action :find_document, except: [:index, :new, :create] 
+
   def index
+    @documents = Document.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @document = Document.new
   end
 
   def create
+    @document = Document.new(document_params)
+
+    if @document.save
+      redirect_to @document
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @document.update(document_params)
+      redirect_to @document
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @document.destroy
+    redirect_to documents_path
   end
 
   private
 
   def find_document
+    @document = Document.find(params[:id])
   end
 
   def document_params
+    params.require(:document).permit(:title, :content)
   end
 end
